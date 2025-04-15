7e071b2942b12d0013891aad5fc5d711e3d832ed
auvy09 <akibalmahmud356@gmail.com>
2025-04-15 22:57:47 +0600

    ADD AND DELETE ADDED

--- src/Comp/Todo.jsx
+++ src/Comp/Todo.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 import img1 from "../assets/todo_icon.png";
 import TodoItems from "./todoItems";
+import { useRef } from "react";
 
 function Todo() {
+  const inputRef = useRef();
+  const [todoList, setTodoList] = useState([]);
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    console.log(inputText);
+    if (inputText === "") {
+      return null;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList((prevTodo) => {
+      return prevTodo.filter((todo) => todo.id !== id);
+    });
+  };
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* title */}
@@ -13,21 +36,31 @@ function Todo() {
       {/* input */}
       <div className="flex items-center my-7 bg-gray-300 rounded-full">
         <input
+          ref={inputRef}
           type="text"
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-400"
           placeholder="Add Task"
         />
-        <button className="border-none rounded-full bg-orange-400 w-32 h-14 text-white text-lg font-medium cursor-pointer hover:bg-orange-700">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-400 w-32 h-14 text-white text-lg font-medium cursor-pointer hover:bg-orange-700"
+        >
           ADD +
         </button>
       </div>
       {/* Todo list */}
       <div>
-        <TodoItems />
-        <TodoItems />
-        <TodoItems />
-        <TodoItems />
-        <TodoItems />
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+              deleteTodo={deleteTodo}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/Comp/todoItems.jsx
+++ src/Comp/todoItems.jsx
@@ -3,14 +3,19 @@ import Tick from "../assets/tick.png";
 import not_Tick from "../assets/not_tick.png";
 import Delete from "../assets/delete.png";
 
-function todoItems() {
+function todoItems({ text, id, isComplete, deleteTodo }) {
   return (
     <div className="flex items-center my-3 gap-2">
       <div className="flex flex-1 items-center cursor-pointer">
         <img src={Tick} className="w-7" alt="" />
-        <p className="text-slate-600 ml-4 text-[16px]">start coading</p>
+        <p className="text-slate-600 ml-4 text-[16px]">{text}</p>
       </div>
-      <img src={Delete} alt="" className="w-3 cursor-pointer" />
+      <img
+        onClick={() => deleteTodo(id)}
+        src={Delete}
+        alt=""
+        className="w-3 cursor-pointer"
+      />
     </div>
   );
 }
